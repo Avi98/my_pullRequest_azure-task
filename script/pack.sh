0a102395697105f847e7fb8d6d17caa9a271323a
Avinash <avinash.kumar@renegadeinsurance.com>
2023-06-21 00:39:13 +0530

    feat: ci pipleine test

--- script/pack.sh
+++ script/pack.sh
@@ -2,9 +2,10 @@
 
 cp -R ../images ../vss-extension.json .dist
 cp -R ../uploadScript .dist/task
-cp -p task.json package.json npm-lock.yaml .npmrc .dist/task 
+cp -p task.json package.json pnpm-lock.yaml .npmrc .dist/task 
 
+chmod -R 700 node_modules
 echo "installing packages for bundle"
 (
- cd .dist/task && pnpm i -P 
+ cd .dist/task && pnpm i -P
 )
